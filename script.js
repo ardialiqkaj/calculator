6bae7bb4ed186c800974307737d52f5a1032fd59
ardialiqkaj <ardi.aliqkaj@gmail.com>
2022-10-21 22:59:05 +0200

    basic math

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
-function addition(...numbers) {
+let firstNum;
+let secondNum;
+let operand;
+
+/*function add(...numbers) {
   let result = numbers.reduce((num1, num2) => num1 + num2);
   return result;
 }
 //console.log(addition(10, 2, 18));
 
-function subtraction(...numbers) {
+function subtrac(...numbers) {
   let result = numbers.reduce((num1, num2) => num1 - num2);
   return result;
 }
 //console.log(subtraction(10, 2, 9));
 
-function multiplication(...numbers) {
+function multiply(...numbers) {
   let result = numbers.reduce((num1, num2) => num1 * num2);
   return result;
 }
 //console.log(multiplication(10, 2));
 
-function division(...numbers) {
+function divise(...numbers) {
   let result = numbers.reduce((num1, num2) => num1 / num2);
   return result;
 }
-//console.log(division(10, 2));
+//console.log(division(10, 2));*/
+
+function operate() {
+  firstNum = prompt("Type the first number:");
+  secondNum = prompt("Type the second number: ");
+  operand = prompt("Type your operand: ");
+
+  if (operand === "+") {
+    result = parseInt(firstNum) + parseInt(secondNum);
+  } else if (operand === "-") {
+    result = parseInt(firstNum) - parseInt(secondNum);
+  } else if (operand === "*") {
+    result = parseInt(firstNum) * parseInt(secondNum);
+  } else if (operand === "/") {
+    result = parseInt(firstNum) / parseInt(secondNum);
+  } else {
+    result = "error";
+  }
+  return result;
+}
+console.log(operate());
+console.log(firstNum);
+console.log(secondNum);
+console.log(operand);
